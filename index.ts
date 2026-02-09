f4d8bf9a6ccb9040045cd7b8ad2b8b9de4114481
pwalczysko <p.walczysko@dundee.ac.uk>
2026-02-09 15:07:36 +0000

    Add artificial coordinates file imported from .js file (replacing the original Australia-centered example from Google)

--- index.ts
+++ index.ts
@@ -4,6 +4,8 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 import { MarkerClusterer } from "@googlemaps/markerclusterer";
+// These .js files are not published on the repository, instead, just example artificial coordinate files are used.
+import { locations } from "./example-coordinates.js"
 
 async function initMap() {
   // Request needed libraries.
@@ -14,7 +16,7 @@ async function initMap() {
     document.getElementById("map") as HTMLElement,
     {
       zoom: 3,
-      center: { lat: -28.024, lng: 140.887 },
+      center: { lat: 28.024, lng: -40.887 },
       mapId: 'DEMO_MAP_ID',
     }
   );
@@ -28,11 +30,18 @@ async function initMap() {
   const labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
   // Add some markers to the map.
+  console.log(locations.length, "new-logging")
   const markers = locations.map((position, i) => {
     const label = labels[i % labels.length];
     const pinGlyph = new google.maps.marker.PinElement({
-      glyph: label,
-      glyphColor: "white",
+      // glyph: label,
+      glyph: "B",
+      glyphColor: "red",
+      background: "white",
+      // glyphText: "some",
+      // glyphSrc: "https://idr.openmicroscopy.org/about/img/logos/logo-idr.svg",
+      // scale: 1.1,
+      // glyphColor: "red",
     })
     const marker = new google.maps.marker.AdvancedMarkerElement({
       position,
@@ -49,34 +58,18 @@ async function initMap() {
   });
 
   // Add a marker clusterer to manage the markers.
-  new MarkerClusterer({ markers, map });
+  new MarkerClusterer({ markers, map, renderer: {
+    render: ({ count, position }) => {
+      return new google.maps.Marker({
+        label: { text: String(count), color: "red" },
+        position,
+        // Customize icon here
+      });
+    },
+  },});
 }
 
-const locations = [
-  { lat: -31.56391, lng: 147.154312 },
-  { lat: -33.718234, lng: 150.363181 },
-  { lat: -33.727111, lng: 150.371124 },
-  { lat: -33.848588, lng: 151.209834 },
-  { lat: -33.851702, lng: 151.216968 },
-  { lat: -34.671264, lng: 150.863657 },
-  { lat: -35.304724, lng: 148.662905 },
-  { lat: -36.817685, lng: 175.699196 },
-  { lat: -36.828611, lng: 175.790222 },
-  { lat: -37.75, lng: 145.116667 },
-  { lat: -37.759859, lng: 145.128708 },
-  { lat: -37.765015, lng: 145.133858 },
-  { lat: -37.770104, lng: 145.143299 },
-  { lat: -37.7737, lng: 145.145187 },
-  { lat: -37.774785, lng: 145.137978 },
-  { lat: -37.819616, lng: 144.968119 },
-  { lat: -38.330766, lng: 144.695692 },
-  { lat: -39.927193, lng: 175.053218 },
-  { lat: -41.330162, lng: 174.865694 },
-  { lat: -42.734358, lng: 147.439506 },
-  { lat: -42.734358, lng: 147.501315 },
-  { lat: -42.735258, lng: 147.438 },
-  { lat: -43.999792, lng: 170.463352 },
-];
+// Here were the original coordinates from google example
 
 initMap();
 export { };
